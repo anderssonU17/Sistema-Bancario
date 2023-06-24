e796b23b42a83d40ad6739ecf2b5f9f44af7ea6d
dcristal-2021434 <107076225+dcristal-2021434@users.noreply.github.com>
2023-06-23 20:31:30 -0600

    Add files via upload

---
+++ banco-react/src/Router.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./styles.css"
+import  AddClient  from "./addClient";
+import Dashboard from "./components/Dashboard";
+
+
+export const Router =()=>{
+    return (
+        <>
+            <section>
+                <Dashboard 
+                    li={[
+                        ["Dashboard", "img/dashboard.svg"],
+                        ["Dashboard", "img/dashboard.svg"],
+                        ["Dashboard", "img/dashboard.svg"],
+                        ["Dashboard", "img/dashboard.svg"]
+                    ]}
+                />
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<AddClient />}></Route>
+                    </Routes>
+                </BrowserRouter>
+            </section>
+        </>
+    );
+};
